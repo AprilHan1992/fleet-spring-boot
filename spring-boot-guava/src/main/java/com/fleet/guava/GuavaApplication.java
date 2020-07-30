9325d2e3ded526fc14d916a0924361a794bae393
AprilHan1992 <785579683@qq.com>
2020-07-30 12:24:04 +0800

    Guava 本地缓存 项目示例

---
+++ spring-boot-guava/src/main/java/com/fleet/guava/GuavaApplication.java
@@ -0,0 +1,14 @@
+package com.fleet.guava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class GuavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuavaApplication.class, args);
+    }
+}
