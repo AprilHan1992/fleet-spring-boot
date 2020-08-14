16dc61fddcefd23bfa7ac353438298bb7d974f39
AprilHan1992 <785579683@qq.com>
2020-08-14 17:56:05 +0800

    结合 Mysql 项目示例

--- spring-boot-mysql/src/main/java/com/fleet/mysql/service/impl/UserServiceImpl.java
+++ spring-boot-mysql/src/main/java/com/fleet/mysql/service/impl/UserServiceImpl.java
@@ -3,25 +3,36 @@ package com.fleet.mysql.service.impl;
 import com.fleet.mysql.entity.User;
 import com.fleet.mysql.service.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author April Han
+ */
 @Service
 public class UserServiceImpl implements UserService {
 
     @Resource
     private JdbcTemplate jdbcTemplate;
 
+    @Resource
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     @Override
     public int insert(User user) {
-        String sql = "insert into `user`(`name`) value (?)";
+        // 方式一
+        String sql = "insert into `user`(`name`) values (?)";
         return jdbcTemplate.update(sql, user.getName());
+
+//        // 方式二
+//        String sql = "insert into `user`(`name`) values (:name)";
+//        Map<String, Object> params = new HashMap<>();
+//        params.put("name", user.getName());
+//        return namedParameterJdbcTemplate.update(sql, params);
     }
 
     @Override
@@ -39,14 +50,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public User get(Long id) {
         String sql = "select `id`, `name` from `user` where `id` = ?";
-        return jdbcTemplate.queryForObject(sql, new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet resultSet, int i) throws SQLException {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                return user;
-            }
+        return jdbcTemplate.queryForObject(sql, (resultSet, i) -> {
+            User user = new User();
+            user.setId(resultSet.getLong("id"));
+            user.setName(resultSet.getString("name"));
+            return user;
         }, id);
     }
 
