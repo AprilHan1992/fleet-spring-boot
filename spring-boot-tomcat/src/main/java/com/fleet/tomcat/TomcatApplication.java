c410c5fbb9aa0c67819cb0e846cdb44715acffcf
AprilHan1992 <785579683@qq.com>
2020-08-19 18:00:55 +0800

    使用 Tomcat （默认） 作为 Servlet 引擎应用 项目示例

---
+++ spring-boot-tomcat/src/main/java/com/fleet/tomcat/TomcatApplication.java
@@ -0,0 +1,12 @@
+package com.fleet.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatApplication.class, args);
+    }
+}
