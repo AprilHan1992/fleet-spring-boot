4ae67e958d46c9d16ded90619c6f171a9c2849c2
AprilHan1992 <785579683@qq.com>
2020-08-31 11:14:42 +0800

    HTTP访问控制

--- spring-boot-security/src/main/java/com/fleet/security/config/CorsConfig.java
+++ spring-boot-security/src/main/java/com/fleet/security/config/CorsConfig.java
@@ -6,10 +6,17 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+/**
+ * 跨域配置
+ *
+ * @author April Han
+ */
 @Configuration
 public class CorsConfig {
 
-    private CorsConfiguration buildConfig() {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         // 跨域的地址，注意 127.0.0.1 != localhost
         corsConfiguration.addAllowedOrigin("*");
@@ -17,13 +24,10 @@ public class CorsConfig {
         corsConfiguration.addAllowedHeader("*");
         // 跨域的请求方法
         corsConfiguration.addAllowedMethod("*");
-        return corsConfiguration;
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig());
+        // 跨域是否支持证书
+        corsConfiguration.setAllowCredentials(true);
+        // 跨域有效接口
+        source.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(source);
     }
 }
