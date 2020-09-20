9ae2283667b628fbe6e9d676aa62c03a93ce892d
AprilHan1992 <785579683@qq.com>
2020-09-20 11:27:00 +0800

    基于JNA的免费的本地操作系统和Java的硬件信息库 项目示例

--- spring-boot-oshi/src/main/java/com/fleet/oshi/controller/InfoController.java
+++ spring-boot-oshi/src/main/java/com/fleet/oshi/controller/InfoController.java
@@ -4,7 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import oshi.SystemInfo;
 import oshi.hardware.*;
+import oshi.software.os.FileSystem;
+import oshi.software.os.NetworkParams;
 import oshi.software.os.OperatingSystem;
+
+import java.util.List;
 
 @RestController
 public class InfoController {
@@ -57,7 +61,7 @@ public class InfoController {
     }
 
     /**
-     * 传感器
+     * 传感器信息
      */
     @RequestMapping("/sensors")
     public Sensors sensors() {
@@ -66,5 +70,89 @@ public class InfoController {
         // 硬件信息
         HardwareAbstractionLayer hal = si.getHardware();
         return hal.getSensors();
+    }
+
+    /**
+     * 电源信息
+     */
+    @RequestMapping("/powerSources")
+    public List<PowerSource> powerSources() {
+        // 系统信息
+        SystemInfo si = new SystemInfo();
+        // 硬件信息
+        HardwareAbstractionLayer hal = si.getHardware();
+        return hal.getPowerSources();
+    }
+
+    /**
+     * 磁盘信息
+     */
+    @RequestMapping("/diskStores")
+    public List<HWDiskStore> diskStores() {
+        // 系统信息
+        SystemInfo si = new SystemInfo();
+        // 硬件信息
+        HardwareAbstractionLayer hal = si.getHardware();
+        return hal.getDiskStores();
+    }
+
+    /**
+     * 文件系统向欧盟西欧
+     */
+    @RequestMapping("/fs")
+    public FileSystem fs() {
+        // 系统信息
+        SystemInfo si = new SystemInfo();
+        // 操作系统信息
+        OperatingSystem os = si.getOperatingSystem();
+        return os.getFileSystem();
+    }
+
+    /**
+     * 网络接口信息
+     */
+    @RequestMapping("/networkIFs")
+    public List<NetworkIF> networkIFs() {
+        // 系统信息
+        SystemInfo si = new SystemInfo();
+        // 硬件信息
+        HardwareAbstractionLayer hal = si.getHardware();
+        return hal.getNetworkIFs();
+    }
+
+    /**
+     * 网络参数信息
+     */
+    @RequestMapping("/networkParams")
+    public NetworkParams networkParams() {
+        // 系统信息
+        SystemInfo si = new SystemInfo();
+        // 操作系统信息
+        OperatingSystem os = si.getOperatingSystem();
+        return os.getNetworkParams();
+    }
+
+    /**
+     * 显示器信息
+     */
+    @RequestMapping("/displays")
+    public List<Display> displays() {
+        // 系统信息
+        SystemInfo si = new SystemInfo();
+        // 硬件信息
+        HardwareAbstractionLayer hal = si.getHardware();
+        return hal.getDisplays();
+    }
+
+    /**
+     * USB设备信息
+     */
+    @RequestMapping("/usbDevices")
+    public List<UsbDevice> usbDevices() {
+        // 系统信息
+        SystemInfo si = new SystemInfo();
+        // 硬件信息
+        HardwareAbstractionLayer hal = si.getHardware();
+        return hal.getUsbDevices(true);
     }
 }
