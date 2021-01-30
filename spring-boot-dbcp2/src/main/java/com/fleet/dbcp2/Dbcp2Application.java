97e2634a0f9d7046851f213fd8ed597654c23c4b
AprilHan1992 <785579683@qq.com>
2021-01-30 14:07:56 +0800

    Dbcp2 连接池 项目示例

---
+++ spring-boot-dbcp2/src/main/java/com/fleet/dbcp2/Dbcp2Application.java
@@ -0,0 +1,15 @@
+package com.fleet.dbcp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author April Han
+ */
+@SpringBootApplication
+public class Dbcp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dbcp2Application.class, args);
+    }
+}
