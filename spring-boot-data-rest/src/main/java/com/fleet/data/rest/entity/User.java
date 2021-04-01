6434f8a9e94a9a94c3efde388fc398a25ea5d961
April-Han <785579683@qq.com>
2021-04-01 22:57:52 +0800

    通过 REST 暴露 Spring Data 数据仓库 项目示例

---
+++ spring-boot-data-rest/src/main/java/com/fleet/data/rest/entity/User.java
@@ -0,0 +1,35 @@
+package com.fleet.data.rest.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
