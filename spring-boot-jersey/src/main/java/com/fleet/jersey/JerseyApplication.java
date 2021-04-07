a552df2f3d7dddf7b436d608d6ff8a148dc1994a
AprilHan1992 <785579683@qq.com>
2021-04-07 12:27:56 +0800

    Jersey Restful 框架 项目示例

---
+++ spring-boot-jersey/src/main/java/com/fleet/jersey/JerseyApplication.java
@@ -0,0 +1,15 @@
+package com.fleet.jersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author April Han
+ */
+@SpringBootApplication
+public class JerseyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JerseyApplication.class, args);
+    }
+}
