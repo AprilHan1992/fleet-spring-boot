d7fc3d1773fc9b00bf7f3b4118779d27d4a69fc7
April-Han <785579683@qq.com>
2021-04-27 21:50:43 +0800

    主题切换 项目示例

---
+++ spring-boot-theme/src/main/java/com/fleet/theme/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.fleet.theme.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new ThemeChangeInterceptor());
+            }
+        };
+    }
+}
--- spring-boot-theme/src/main/java/com/fleet/theme/controller/IndexController.java
+++ spring-boot-theme/src/main/java/com/fleet/theme/controller/IndexController.java
@@ -1,13 +1,7 @@
 package com.fleet.theme.controller;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ThemeResolver;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author April Han
@@ -15,14 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class IndexController {
 
-    @Resource
-    private ThemeResolver themeResolver;
-
     @RequestMapping("/index")
-    public String index(HttpServletRequest request, HttpServletResponse response, String theme) {
-        if (StringUtils.isNotEmpty(theme)) {
-            themeResolver.setThemeName(request, response, theme);
-        }
+    public String index() {
         return "index";
     }
+
+//    @Resource
+//    private ThemeResolver themeResolver;
+//
+//    @RequestMapping("/index")
+//    public String index(HttpServletRequest request, HttpServletResponse response, String theme) {
+//        if (StringUtils.isNotEmpty(theme)) {
+//            themeResolver.setThemeName(request, response, theme);
+//        }
+//        return "index";
+//    }
 }
