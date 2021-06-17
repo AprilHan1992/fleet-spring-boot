42537700f448238562aeb652dfd7f22b732acdc9
AprilHan1992 <785579683@qq.com>
2021-06-17 14:47:26 +0800

    结合 Mybatis PageHelper 项目示例

--- spring-boot-mybatis-pagehelper/src/test/java/com/fleet/mybatis/pagehelper/MybatisPageHelperApplicationTests.java
+++ spring-boot-mybatis-pagehelper/src/test/java/com/fleet/mybatis/pagehelper/MybatisPageHelperApplicationTests.java
@@ -1,15 +1,27 @@
 package com.fleet.mybatis.pagehelper;
 
+import com.fleet.mybatis.pagehelper.entity.User;
+import com.fleet.mybatis.pagehelper.page.PageUtil;
+import com.fleet.mybatis.pagehelper.page.entity.Page;
+import com.fleet.mybatis.pagehelper.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MybatisPageHelperApplicationTests {
 
+    @Resource
+    UserService userService;
+
     @Test
-    public void contextLoads() {
+    public void listPage() {
+        Page page = new Page(3, 5);
+        PageUtil<User> pageUtil = userService.listPage(page);
+        System.out.println(pageUtil);
     }
 }
